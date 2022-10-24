38ccd2e5e0096079e1dcf46fa4e3baf74375284f
oscarboe <Oscarboen@gmail.com>
2022-10-24 12:56:27 +0200

    Update

---
+++ Database/Database_test1/Database_test1/Data/DBContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Database_test1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database_test1.Data
+{
+    public class PortfolioDbContext : DbContext
+    {
+        public PortfolioDbContext(DbContextOptions<PortfolioDbContext> options)
+            : base(options) { }
+
+        public DbSet<Text> Texts { get; set; }
+
+
+    }
+}
+
+/*public class MyDBContext : DbContext, IMyDBContext
+   {
+   DbSet<Texts> Texts { get; set; }
+   
+   
+   public string DbPath { get; }
+   
+   
+   // The following configures EF to create a Sqlite database file in the
+   // special "local" folder for your platform.
+   protected override void OnConfiguring(DbContextOptionsBuilder options)
+   => options.UseSqlServer($"Data Source=127.0.0.1,1433;Database=sql1;User Id=sa;Password=Strong123;TrustServerCertificate=True\r\n");
+   }*/
---
+++ Database/Database_test1/Database_test1/Models/DBTables.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_test1.Models
+{
+    public interface IText
+    {
+        public int TextID { get; set; }
+        public string Headline { get; set; }
+
+        public string MainText { get; set; }
+    }
+
+    public class Text : IText
+    {
+        [Key]
+        public int TextID { get; set; }
+
+        [MaxLength(50)]
+        public string Headline { get; set; }
+
+        public string MainText { get; set; }
+
+    }
+
+}
