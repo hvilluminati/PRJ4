60ceb3d15262d736beca78d35d164af50c9a609d
oscarboe <Oscarboen@gmail.com>
2022-10-24 13:30:47 +0200

    skills created

--- Database/Database_test1/Database_test1/Data/DBContext.cs
+++ Database/Database_test1/Database_test1/Data/DBContext.cs
@@ -14,20 +14,8 @@ namespace Database_test1.Data
 
         public DbSet<Text> Texts { get; set; }
 
+        public DbSet<Skill> Skills { get; set; }
+
 
     }
 }
-
-/*public class MyDBContext : DbContext, IMyDBContext
-   {
-   DbSet<Texts> Texts { get; set; }
-   
-   
-   public string DbPath { get; }
-   
-   
-   // The following configures EF to create a Sqlite database file in the
-   // special "local" folder for your platform.
-   protected override void OnConfiguring(DbContextOptionsBuilder options)
-   => options.UseSqlServer($"Data Source=127.0.0.1,1433;Database=sql1;User Id=sa;Password=Strong123;TrustServerCertificate=True\r\n");
-   }*/
