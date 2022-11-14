bd5996b79f67f606770ee3bf7b6b907d472ebf58
lassehj0 <lassehj00@github.com>
2022-11-14 13:12:51 +0100

    lotta changes

--- Frontend/src/pages/aboutPage.tsx
+++ Frontend/src/pages/aboutPage.tsx
@@ -1,85 +1,15 @@
-import React from 'react';
-//import { Link } from 'react-router-dom';
-import axios from 'axios';
 import picture from '../nonbinary-person.png';
+import { Link } from 'react-router-dom';
 
 function About() {
-	var desc;
-	var lang: string[] = [
-		'React',
-		'GraphQL',
-		'CSharp',
-		'CSharp1',
-		'CSharp2',
-		'CSharp3',
-		'CSharp4',
-		'CSharp5',
-		'CSharp6',
-		'CSharp7',
-		'CSharp8',
-		'CSharp9',
-	];
-	var pos = [
-		[100, 100],
-		[1236, 640],
-		[170, 540],
-		[1264, 125],
-		[628, 100],
-		[565, 486],
-		[265, 345],
-		[413, 629],
-		[1186, 397],
-		[853, 579],
-		[218, 130],
-		[886, 397],
-	];
-
-	const axiosInstance = axios.create();
-	axiosInstance
-		.get('idk')
-		.then((response) => {
-			desc = response.data;
-		})
-		.catch(console.error);
-
-	axiosInstance
-		.get('languages')
-		.then((response) => {
-			lang = response.data;
-			response.data.each((i: number, elem: string) => {
-				var p = document.createElement('p');
-				p.className = 'scribble';
-				p.id = elem;
-				p.innerHTML = elem;
-				document.getElementById('langs')?.appendChild(p);
-			});
-		})
-		.catch(console.error);
-
-	document.onmousemove = function (e) {
-		var x = e.clientX;
-		var y = e.clientY;
-		lang.forEach((entry, i) => {
-			var style = document.getElementById(entry)?.style;
-			if (style != null) {
-				style.marginLeft =
-					([1, 4, 5, 8, 10, 14, 15, 16, 19, 21].indexOf(i) != -1
-						? pos[i][0] + x / 20
-						: pos[i][0] - x / 20) + 'px';
-
-				style.marginTop =
-					([2, 4, 7, 9, 10, 11, 15, 16, 17, 19, 22].indexOf(i) != -1
-						? pos[i][1] + y / 20
-						: pos[i][1] - y / 20) + 'px';
-
-				style.position = 'absolute';
-			}
-		});
-	};
-
 	return (
 		<>
 			<div id='hej'>
+				<Link to='/'>
+					<button className='button button1'>
+						<span>Home</span>
+					</button>{' '}
+				</Link>
 				<div id='about'>
 					<div id='title'>
 						<h1>About me</h1>
@@ -115,46 +45,7 @@ function About() {
 							</div>
 						</div>
 					</div>
-					<div id='floats'></div>
 				</div>
-			</div>
-			<div id='langs'>
-				<p className='scribble' id='React'>
-					React
-				</p>
-				<p className='scribble' id='GraphQL'>
-					GraphQl
-				</p>
-				<p className='scribble' id='CSharp'>
-					C#
-				</p>
-				<p className='scribble' id='CSharp1'>
-					C#1
-				</p>
-				<p className='scribble' id='CSharp2'>
-					C#2
-				</p>
-				<p className='scribble' id='CSharp3'>
-					C#3
-				</p>
-				<p className='scribble' id='CSharp4'>
-					C#4
-				</p>
-				<p className='scribble' id='CSharp5'>
-					C#5
-				</p>
-				<p className='scribble' id='CSharp6'>
-					C#6
-				</p>
-				<p className='scribble' id='CSharp7'>
-					C#7
-				</p>
-				<p className='scribble' id='CSharp8'>
-					C#8
-				</p>
-				<p className='scribble' id='CSharp9'>
-					C#9
-				</p>
 			</div>
 		</>
 	);
--- Frontend/src/pages/skillsPage.tsx
+++ Frontend/src/pages/skillsPage.tsx
@@ -1,94 +1,30 @@
-import axios from "axios";
-import React from "react";
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 //https://codepen.io/ryasan86/pen/bGpqdYV
 function Skills() {
-  var desc;
-  const texts: string[] = [
-    "React",
-    "GraphQL",
-    "TypeScript",
-    "Prisma",
-    "Apollo",
-    "NodeJS",
-    "Express",
-    "JWT",
-    "Figma",
-    "Git",
-    "Netlify",
-    "Heroku",
-    "Angular",
-    "SSR",
-    "_Lodash",
-    "Python",
-    "Redux",
-    "REST",
-    "Cloudinary",
-    "Firebase",
-    "SCSS",
-    "TDD",
-    "Ionic",
-  ];
-
-  document.onmousemove = function (e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    document.getElementById("cs")!.style.marginLeft = 100 + x / 10 + "px";
-    document.getElementById("cs")!.style.marginTop = 100 + y / 10 + "px";
-    document.getElementById("React")!.style.marginLeft = 100 + x / 10 + "px";
-    document.getElementById("React")!.style.marginTop = 100 + y / 10 + "px";
-  };
-
-  var desc;
-  var lang: string[];
-  const axiosInstance = axios.create();
-  axiosInstance
-    .get("idk")
-    .then((response) => {
-      desc = response.data;
-    })
-    .catch(console.error);
-
-  axiosInstance
-    .get("languages")
-    .then((response) => {
-      response.data.each((i: number, elem: string) => {
-        var p = document.createElement("p");
-        p.innerHTML = elem;
-        document.getElementById("langs")?.appendChild(p);
-      });
-    })
-    .catch(console.error);
-
-  const code = (name: string) => {
-    var element = document.getElementById(name);
-    element!.style.opacity = "1";
-  };
-
-  return (
-    <>
-      <div id="hej">
-        <div id="skills">
-          <h1 id="title">Skills</h1>
-          <Link to="/">
-            <button id="homePageBtn">HomePage</button>
-          </Link>
-          <div id="codeNames">{JSON.stringify(texts)}</div>
-          <div id="codeNames"> texts </div>
-        </div>
-      </div>
-      <a className="scribble" id="cs" onClick={() => code("reactBox")}>
-        C#
-      </a>
-      <a className="scribble" id="React" onClick={() => code("reactBox")}>
-        React
-      </a>
-      <div className="skillsBox" id="csBox"></div>
-      <div className="skillsBox" id="reactBox"></div>
-    </>
-  );
+	return (
+		<>
+			<div id='hej'>
+				<Link to='/'>
+					<button className='button button1'>
+						<span>Home</span>
+					</button>{' '}
+				</Link>
+				<div id='skills'>
+					<div id='title'>
+						<h1>Skills</h1>
+					</div>
+					<div id='skillText1'>Press a code language</div>
+					<div id='skillText2'>Hover the bar to see more information</div>
+				</div>
+				<div id='skillLVL'>
+					Skill lvl: Noob <br></br> Experience: <br></br> 2 days
+				</div>
+				<div id='codeBox'></div>
+			</div>
+		</>
+	);
 }
 
 export default Skills;
