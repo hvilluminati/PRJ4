8585c1e04fc48b3ddec3166badb47c9d9268169c
lassehj0 <lassehj00@github.com>
2022-11-28 13:14:32 +0100

    First commit

---
+++ Database_test1/Data/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Database_test1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database_test1.Data
+{
+    public class PortfolioDbContext : DbContext
+    {
+        public PortfolioDbContext(DbContextOptions<PortfolioDbContext> options)
+            : base(options) { }
+
+        public DbSet<Text> Texts { get; set; }
+
+        public DbSet<Skill> Skills { get; set; }
+    }
+}
