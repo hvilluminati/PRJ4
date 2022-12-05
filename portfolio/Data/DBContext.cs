1eeea557416e5766a39b01b6df38621590142372
lassehj0 <lassehj00@github.com>
2022-12-05 13:22:56 +0100

    first

---
+++ portfolio/Data/DBContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class PortfolioDbContext : DbContext
+    {
+        public PortfolioDbContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Text> Texts { get; set; }
+
+        public DbSet<Skill> Skills { get; set; }
+
+
+
+        public string DbPath { get; }
+
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+       }
+}
