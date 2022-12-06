1f28a9483d5c884ded9de19dad071b7164d0fa77
oscarboe <Oscarboen@gmail.com>
2022-12-07 00:20:53 +0100

    mapped projects data to table and deleted helper function

--- portfolio/client-app/src/pages/projectsPage.tsx
+++ portfolio/client-app/src/pages/projectsPage.tsx
@@ -36,46 +36,25 @@ export default function Projects() {
       }
     });
   }, []);
-  function addRow(tableID: string, fileinfo: any) {
-    console.log(fileinfo);
-    var table = document.getElementById(tableID) as HTMLTableElement;
-    if (table) {
-      var rowCount = table.rows.length;
-      var row = table.insertRow(rowCount);
-      var numberOfEntries = fileinfo.length;
-      var cell1 = row.insertCell(0);
-      cell1.innerHTML = fileinfo[0].name;
-
-      var cell2 = row.insertCell(1);
-      cell2.innerHTML = fileinfo[0].fileType;
-
-      var cell3 = row.insertCell(2);
-      cell3.innerHTML = fileinfo[0].language;
-
-      var cell4 = row.insertCell(3);
-      let idref = fileinfo[0].id.tostring; //
-      let buttonString = `<button onClick={() => getBlob(${idref})}>Download</button>`;
-
-      cell4.innerHTML = buttonString;
-    }
-  }
 
   return (
     <div>
-      <input
-        type='button'
-        value='Add Row'
-        onClick={() => addRow('dataTable', fileinfo)}
-      />
-
       <table id='dataTable' width='350px'>
         <tr>
-          <td>{'name'}</td>
+          <td>{'Encrypted Project name'}</td>
           <td>{'File Type'}</td>
-          <td>{'Programming language'}</td>
-          <td>{'Download Button'}</td>
-          <button onClick={() => getBlob('1')}>Download</button>'
+          <td>{'Coding language'}</td>
+          <td>{'Download button'}</td>
         </tr>
+
+        {fileinfo.map((f) => (
+          <tr>
+            <td>{f.name}</td>
+            <td>{f.fileType}</td>
+            <td>{f.language}</td>
+            <button onClick={() => getBlob(f.id)}>Download</button>'
+          </tr>
+        ))}
       </table>
     </div>
   );
