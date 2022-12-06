947da88e45d5047b985623fa292efec747c8ba28
oscarboe <Oscarboen@gmail.com>
2022-12-06 16:11:51 +0100

    succeeded in loading the data into the array

--- portfolio/client-app/src/pages/projectsPage.tsx
+++ portfolio/client-app/src/pages/projectsPage.tsx
@@ -7,29 +7,32 @@ export default function Projects() {
   //const [language, setLanguage] = useState(['unknown Language']);
   //const [type, setType] = useState([' unknown type']);
   //const [name, setName] = useState([' unknown name']);
-  const [fileinfo, setFileinfo] = useState<
-    Array<{
-      id: string;
-      name: string;
-      fileType: string;
-      language: string;
-    }>
-  >([]);
+  // const [fileinfo, setFileinfo] = useState<
+  //   Array<{
+  //     id: string;
+  //     name: string;
+  //     fileType: string;
+  //     language: string;
+  //   }>
+  // >([]);
+  const [fileinfo, setFileinfo] = useState([{}]);
 
   useEffect(() => {
     getFiles().then((response) => {
-      setFileinfo(response[0]);
-      // for (let index = 0; index < response.length; index++) {
-      setFileinfo((arr) => [
-        {
-          id: response.id,
-          name: response.name,
-          fileType: response.fileType,
-          language: response.language,
-        },
-      ]);
+      setFileinfo(response.data.result);
+      console.log('test3', fileinfo);
+      // setFileinfo(response[0]);
+      // // for (let index = 0; index < response.length; index++) {
+      // setFileinfo((arr) => [
+      //   {
+      //     id: response.id,
+      //     name: response.name,
+      //     fileType: response.fileType,
+      //     language: response.language,
+      //   },
+      // ]);
       //}
-      console.log('test2', fileinfo);
+
       /*
       setLanguage(response.language[0]);
       console.log('language contains', language);
@@ -43,20 +46,20 @@ export default function Projects() {
       */
     });
   }, []);
-  function addRow(tableID: string) {
+  function addRow(tableID: string, fileinfo: any) {
     var table = document.getElementById(tableID) as HTMLTableElement;
     if (table) {
       var rowCount = table.rows.length;
       var row = table.insertRow(rowCount);
-
+      var numberOfEntries = fileinfo.length;
       var cell1 = row.insertCell(0);
       cell1.innerHTML = fileinfo[0].name;
 
       var cell2 = row.insertCell(1);
-      cell2.innerHTML = fileinfo[0].fileType;
+      cell2.innerHTML = fileinfo[0];
 
       var cell3 = row.insertCell(2);
-      cell3.innerHTML = fileinfo[0].language;
+      cell3.innerHTML = fileinfo[0];
 
       var cell4 = row.insertCell(3);
 
@@ -71,7 +74,7 @@ export default function Projects() {
       <input
         type='button'
         value='Add Row'
-        onClick={() => addRow('dataTable')}
+        onClick={() => addRow('dataTable', fileinfo)}
       />
 
       <table id='dataTable' width='350px'>
