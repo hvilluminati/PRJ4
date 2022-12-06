490f84aa0b42ebf0484f549fd72670cc69737644
oscarboe <Oscarboen@gmail.com>
2022-12-06 17:49:59 +0100

    trying to get download button to work

--- portfolio/client-app/src/pages/projectsPage.tsx
+++ portfolio/client-app/src/pages/projectsPage.tsx
@@ -4,79 +4,79 @@ import { Link } from 'react-router-dom';
 import { count } from 'console';
 
 export default function Projects() {
-	const [fileinfo, setFileinfo] = useState<
-		Array<{
-			id: string;
-			name: string;
-			fileType: string;
-			language: string;
-		}>
-	>([]);
+  const [fileinfo, setFileinfo] = useState<
+    Array<{
+      id: string;
+      name: string;
+      fileType: string;
+      language: string;
+    }>
+  >([]);
 
-	useEffect(() => {
-		getFiles().then((response) => {
-			setFileinfo([
-				{
-					id: response[0].documentId,
-					name: response[0].name,
-					fileType: response[0].fileType,
-					language: response[0].language,
-				},
-			]);
-			for (let index = 0; index < response.length; index++) {
-				setFileinfo((arr) => [
-					...arr,
-					{
-						id: response[index].documentId,
-						name: response[index].name,
-						fileType: response[index].fileType,
-						language: response[index].language,
-					},
-				]);
-			}
-		});
-	}, []);
-	function addRow(tableID: string, fileinfo: any) {
-		console.log(fileinfo);
-		var table = document.getElementById(tableID) as HTMLTableElement;
-		if (table) {
-			var rowCount = table.rows.length;
-			var row = table.insertRow(rowCount);
-			var numberOfEntries = fileinfo.length;
-			var cell1 = row.insertCell(0);
-			cell1.innerHTML = fileinfo[0].name;
+  useEffect(() => {
+    getFiles().then((response) => {
+      setFileinfo([
+        {
+          id: response[0].documentId,
+          name: response[0].name,
+          fileType: response[0].fileType,
+          language: response[0].language,
+        },
+      ]);
+      for (let index = 0; index < response.length; index++) {
+        setFileinfo((arr) => [
+          ...arr,
+          {
+            id: response[index].documentId,
+            name: response[index].name,
+            fileType: response[index].fileType,
+            language: response[index].language,
+          },
+        ]);
+      }
+    });
+  }, []);
+  function addRow(tableID: string, fileinfo: any) {
+    console.log(fileinfo);
+    var table = document.getElementById(tableID) as HTMLTableElement;
+    if (table) {
+      var rowCount = table.rows.length;
+      var row = table.insertRow(rowCount);
+      var numberOfEntries = fileinfo.length;
+      var cell1 = row.insertCell(0);
+      cell1.innerHTML = fileinfo[0].name;
 
-			var cell2 = row.insertCell(1);
-			cell2.innerHTML = fileinfo[0];
+      var cell2 = row.insertCell(1);
+      cell2.innerHTML = fileinfo[0].fileType;
 
-			var cell3 = row.insertCell(2);
-			cell3.innerHTML = fileinfo[0];
+      var cell3 = row.insertCell(2);
+      cell3.innerHTML = fileinfo[0].language;
 
-			var cell4 = row.insertCell(3);
+      var cell4 = row.insertCell(3);
+      let idref = fileinfo[0].id.tostring; //
+      let buttonString = `<button onClick={() => getBlob(${idref})}>Download</button>`;
 
-			//let buttonString = `<button onClick={() => getBlob(${id})}>Download</button>`;
+      cell4.innerHTML = buttonString;
+    }
+  }
 
-			//cell4.innerHTML = buttonString;
-		}
-	}
+  return (
+    <div>
+      <input
+        type='button'
+        value='Add Row'
+        onClick={() => addRow('dataTable', fileinfo)}
+      />
 
-	return (
-		<div>
-			<input
-				type='button'
-				value='Add Row'
-				onClick={() => addRow('dataTable', fileinfo)}
-			/>
-
-			<table id='dataTable' width='350px'>
-				<tr>
-					<td>{'name'}</td>
-					<td>{'File Type'}</td>
-					<td>{'Programming language'}</td>
-					<td>{'Download Button'}</td>
-					<button onClick={() => getBlob('1')}>Download</button>'
-				</tr>
-			</table>
-		</div>
-	);
+      <table id='dataTable' width='350px'>
+        <tr>
+          <td>{'name'}</td>
+          <td>{'File Type'}</td>
+          <td>{'Programming language'}</td>
+          <td>{'Download Button'}</td>
+          <button onClick={() => getBlob('1')}>Download</button>'
+        </tr>
+      </table>
+    </div>
+  );
 }
