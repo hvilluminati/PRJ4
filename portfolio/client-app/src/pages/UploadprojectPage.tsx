24f3a45068156f7b2f0c03eb61e2df1f62e33586
Heja Hassan <83606847+MaziatHassan@users.noreply.github.com>
2022-12-09 10:49:35 +0100

    design af uploadprojekt

--- portfolio/client-app/src/pages/UploadprojectPage.tsx
+++ portfolio/client-app/src/pages/UploadprojectPage.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { postProject } from '../axioscalls';
 import { Link } from 'react-router-dom';
 
+
+
 export default function Upload() {
   const [selectedFile, setSelectedFile] = useState<any>([]);
   const [language, setLanguage] = useState('no Language');
@@ -16,11 +18,14 @@ export default function Upload() {
   };
   return (
     <div>
+       <div  id='hej'>
       <Link to='/'>
         <button className='button button1'>
           <span>Home</span>
         </button>{' '}
       </Link>
+      </div>
+      <div  id='Selectfile'>
       <input type='file' name='file' onChange={fileUploaded} />
       <input
         type='string'
@@ -40,9 +45,11 @@ export default function Upload() {
       ) : (
         <p>Select a file to show details</p>
       )}
-      <div>
+      <div id='submit'>
         <button onClick={Submit}>Submit</button>
       </div>
+      </div>
     </div>
+    
   );
 }
