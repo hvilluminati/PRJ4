2cf9f66dca12729f1eb1956e2b7c3e96bf46f818
lassehj0 <100580392+lassehj0@users.noreply.github.com>
2022-12-13 21:14:35 +0100

    fsdf

--- portfolio/client-app/src/__test__/projektPage.test.tsx
+++ portfolio/client-app/src/__test__/projektPage.test.tsx
@@ -1,102 +1,88 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import Projects from "../pages/projectsPage";
-import { JSDOM } from "jsdom";
-import { fireEvent, screen } from "@testing-library/react";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import { mockComponent } from "react-dom/test-utils";
-import Button from "react-bootstrap/esm/Button";
-import axios from "axios";
-import { render, waitFor } from "@testing-library/react";
-
-describe(" test of projekt page", () => {
-  it("Project", () => {
-    render(
-      <BrowserRouter>
-        <Projects />
-      </BrowserRouter>
-    );
-
-    screen.debug();
-  });
-});
-
-jest.mock("axios");
-const mockedAxios = axios as jest.Mocked<typeof axios>;
+jest.mock('../axioscalls');
+import Projects from '../pages/projectsPage';
+import { fireEvent, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { render, waitFor } from '@testing-library/react';
+import { getFiles } from '../axioscalls';
+import { Dropdown } from 'react-bootstrap';
 
 const dummyProjects = [
-  {
-    id: "1",
-    name: "file1.jpg",
-    fileType: "jpg",
-    language: "js",
-  },
-  {
-    id: "2",
-    name: "file1.jpg",
-    fileType: "jpg",
-    language: "js",
-  },
-  {
-    id: "3",
-    name: "file1.jpg",
-    fileType: "jpg",
-    language: "js",
-  },
-  {
-    id: "4",
-    name: "file1.jpg",
-    fileType: "jpg",
-    language: "js",
-  },
+	{
+		id: '1',
+		name: 'file1.jpg',
+		fileType: 'jpg',
+		language: 'js',
+	},
+	{
+		id: '2',
+		name: 'file1.jpg',
+		fileType: 'jpg',
+		language: 'js',
+	},
+	{
+		id: '3',
+		name: 'file1.jpg',
+		fileType: 'jpg',
+		language: 'js',
+	},
+	{
+		id: '4',
+		name: 'file1.jpg',
+		fileType: 'jpg',
+		language: 'js',
+	},
 ];
 
-test("dropdown button on screen", async () => {
-  mockedAxios.get.mockResolvedValue(dummyProjects);
-  const screen = render(
-    <BrowserRouter>
-      <Projects />
-    </BrowserRouter>
-  );
+const mockPutTitle = getFiles as jest.Mock;
+mockPutTitle.mockResolvedValue(dummyProjects);
 
-  const buttonClicked = await waitFor(() =>
-    screen.findByDisplayValue("Dropdown button")
-  );
-  expect(buttonClicked).toHaveLength(1);
-});
-test("dropdown button click simulation", async () => {
-  mockedAxios.get.mockResolvedValue(dummyProjects);
-  const screen = render(
-    <BrowserRouter>
-      <Projects />
-    </BrowserRouter>
-  );
+test('dropdown button on screen', async () => {
+	const { getAllByText } = render(
+		<BrowserRouter>
+			<Projects />
+		</BrowserRouter>
+	);
 
-  const buttonClicked = await waitFor(() =>
-    screen.findByDisplayValue("Dropdown button")
-  );
-  expect(buttonClicked).toHaveBeenCalled();
-});
-test("projects list", async () => {
-  mockedAxios.get.mockResolvedValue(dummyProjects);
-  const screen = render(
-    <BrowserRouter>
-      <Projects />
-    </BrowserRouter>
-  );
-
-  const projectsList = await waitFor(() => screen.findAllByTestId("Titel"));
-  expect(projectsList).toHaveLength(4);
+	expect(getAllByText('Dropdown button')).toBeInTheDocument;
+	expect(getAllByText('Dropdown button')).toHaveLength(1);
 });
 
-test("download button", async () => {
-  mockedAxios.get.mockResolvedValue(dummyProjects);
-  const screen = render(
-    <BrowserRouter>
-      <Projects />
-    </BrowserRouter>
-  );
+test('dropdown button click simulation', async () => {
+	const { getByText } = render(
+		<BrowserRouter>
+			<Projects />
+		</BrowserRouter>
+	);
 
-  const projectsList = await waitFor(() => screen.findAllByTestId("Download"));
-  expect(projectsList);
+	await new Promise((r) => setTimeout(r, 1000));
+
+	fireEvent.click(getByText('Dropdown button'));
+
+	await new Promise((r) => setTimeout(r, 1000));
+
+	expect(getByText('Sort by name')).toBeInTheDocument;
+});
+
+test('projects list', async () => {
+	const screen = render(
+		<BrowserRouter>
+			<Projects />
+		</BrowserRouter>
+	);
+
+	const projectsList = await waitFor(() => screen.findAllByTestId('Titel'));
+	expect(projectsList).toHaveLength(4);
+});
+
+test('download button', async () => {
+	const screen = render(
+		<BrowserRouter>
+			<Projects />
+		</BrowserRouter>
+	);
+
+	const projectsList = await waitFor(() =>
+		screen.findAllByTestId('Download')
+	);
+	expect(projectsList);
 });
