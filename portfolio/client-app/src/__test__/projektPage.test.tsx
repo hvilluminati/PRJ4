25fab3dfa963ce18d60242fe21cfefdfb766cfb8
Heja Hassan <83606847+MaziatHassan@users.noreply.github.com>
2022-12-14 12:54:25 +0100

    Testproject

--- portfolio/client-app/src/__test__/projektPage.test.tsx
+++ portfolio/client-app/src/__test__/projektPage.test.tsx
@@ -1,81 +1,98 @@
-jest.mock('../axioscalls');
-import Projects from '../pages/projectsPage';
-import { fireEvent, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import { render, waitFor } from '@testing-library/react';
-import { getFiles } from '../axioscalls';
+jest.mock("../axioscalls");
+jest.spyOn(console, "error").mockImplementation(() => {});
+
+import Projects from "../pages/projectsPage";
+import { fireEvent, getByText, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { render, waitFor } from "@testing-library/react";
+import { getFiles } from "../axioscalls";
 
 const dummyProjects = [
-	{
-		id: '1',
-		name: 'file1.jpg',
-		fileType: 'jpg',
-		language: 'js',
-	},
-	{
-		id: '2',
-		name: 'file1.jpg',
-		fileType: 'jpg',
-		language: 'js',
-	},
-	{
-		id: '3',
-		name: 'file1.jpg',
-		fileType: 'jpg',
-		language: 'js',
-	},
-	{
-		id: '4',
-		name: 'file1.jpg',
-		fileType: 'jpg',
-		language: 'js',
-	},
+  {
+    id: "1",
+    name: "file1.jpg",
+    fileType: "jpg",
+    language: "js",
+  },
+  {
+    id: "2",
+    name: "file1.jpg",
+    fileType: "jpg",
+    language: "js",
+  },
+  {
+    id: "3",
+    name: "file1.jpg",
+    fileType: "jpg",
+    language: "js",
+  },
+  {
+    id: "4",
+    name: "file1.jpg",
+    fileType: "jpg",
+    language: "js",
+  },
 ];
 
 const mockPutTitle = getFiles as jest.Mock;
 mockPutTitle.mockResolvedValue(dummyProjects);
 
-test('dropdown button on screen', async () => {
-	const { getAllByText } = render(
-		<BrowserRouter>
-			<Projects />
-		</BrowserRouter>
-	);
+test("dropdown button on screen", async () => {
+  const { getAllByText } = render(
+    <BrowserRouter>
+      <Projects />
+    </BrowserRouter>
+  );
 
-	expect(getAllByText('Dropdown button')).toBeInTheDocument;
-	expect(getAllByText('Dropdown button')).toHaveLength(1);
+  expect(getAllByText("Dropdown button")).toBeInTheDocument;
+  expect(getAllByText("Dropdown button")).toHaveLength(1);
 });
 
-test('dropdown button click simulation', async () => {
-	const { getByText } = render(
-		<BrowserRouter>
-			<Projects />
-		</BrowserRouter>
-	);
+test("dropdown button click simulation", async () => {
+  const { getByText } = render(
+    <BrowserRouter>
+      <Projects />
+    </BrowserRouter>
+  );
 
-	fireEvent.click(getByText('Dropdown button'));
+  fireEvent.click(getByText("Dropdown button"));
 
-	expect(getByText('Sort By Name')).toBeInTheDocument;
-	expect(getByText('Sort By Date')).toBeInTheDocument;
-	expect(getByText('Find all C# Projects')).toBeInTheDocument;
+  expect(getByText("Sort By Name")).toBeInTheDocument;
+  expect(getByText("Sort By Date")).toBeInTheDocument;
+  expect(getByText("Find all C# Projects")).toBeInTheDocument;
 });
 
-test('projects list', async () => {
-	const screen = render(
-		<BrowserRouter>
-			<Projects />
-		</BrowserRouter>
-	);
+test("projects list", async () => {
+  const screen = render(
+    <BrowserRouter>
+      <Projects />
+    </BrowserRouter>
+  );
 
-	expect(screen.findAllByText('Encrypted Project name')).toBeInTheDocument;
+  expect(screen.findAllByText("Encrypted Project name")).toBeInTheDocument;
 });
 
-test('download button', async () => {
-	const screen = render(
-		<BrowserRouter>
-			<Projects />
-		</BrowserRouter>
-	);
+test("download button", async () => {
+  const screen = render(
+    <BrowserRouter>
+      <Projects />
+    </BrowserRouter>
+  );
 
-	expect(screen.findAllByTestId('Download'));
+  expect(screen.findAllByTestId("Download"));
+});
+
+///////Function////////////
+
+describe("Test table", () => {
+  it("Should set id, name, fileType and language", async () => {
+    const { getByText, getAllByText } = render(
+      <BrowserRouter>
+        <Projects />
+      </BrowserRouter>
+    );
+
+    expect(getByText("Download")).toBeInTheDocument;
+    expect(getAllByText("Download")).toHaveLength(4);
+  });
 });
