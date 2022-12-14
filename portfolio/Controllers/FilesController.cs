a4eebbc41404e857f09b048ea9857376eeac8575
oscarboe <Oscarboen@gmail.com>
2022-12-15 00:05:34 +0100

    Added 3 additional back-end integration tests

--- portfolio/Controllers/FilesController.cs
+++ portfolio/Controllers/FilesController.cs
@@ -82,37 +82,7 @@ namespace Database_test1.Controllers
 
             
         }
-
-        // PUT: api/Files/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutFiles(int id, Files files)
-        {
-            if (id != files.DocumentId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(files).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!FilesExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
+        
 
         // POST: api/Files
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
@@ -128,8 +98,6 @@ namespace Database_test1.Controllers
                     var fileName = Path.GetFileName(files.FileName);
                     //Getting file Extension
                     var fileExtension = Path.GetExtension(fileName);
-                    // concatenating  FileName + FileExtension
-                    //var newFileName = String.Concat(Convert.ToString(Guid.NewGuid()), fileExtension);
 
                     //Check if file with same name already exists
                     if ( (_context.Files.Where(x => x.Name == fileName).Any()==false))
@@ -144,7 +112,7 @@ namespace Database_test1.Controllers
                         Language = language,
                     };
 
-                    using (var target = new MemoryStream())
+                    using (var target = new MemoryStream()) 
                     {
                         files.CopyTo(target);
                         objfiles.DataFiles = target.ToArray();
